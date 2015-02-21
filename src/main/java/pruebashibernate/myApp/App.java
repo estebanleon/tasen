ac809bced9a9d3c651b6c4b68970f383f0ae5e28
estebanleon <estebanleon1982@gmail.com>
2015-02-21 19:17:05 +0100

    prueba

---
+++ src/main/java/pruebashibernate/myApp/App.java
@@ -0,0 +1,45 @@
+package pruebashibernate.myApp;
+
+import java.io.File;
+import java.util.List;
+import org.hibernate.HibernateException; 
+import org.hibernate.Session; 
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static SessionFactory factory;
+		Session s = SessionFactoriU
+	
+    public static void main( String[] args )
+    {
+    	
+    	try{
+            factory = new Configuration().configure().buildSessionFactory();
+         }catch (Throwable ex) { 
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex); 
+         }
+    	App AP = new App();
+    	AP.listCentros();
+        System.out.println( "Hello World!" );
+    }
+    
+    public void listCentros()
+    {
+    	Session session = factory.openSession();
+    	Transaction tx = null;
+    	try{
+    		tx = session.beginTransaction();
+    		List centros = session.createQuery("FROM centro").list();
+    	}
+    	finally
+    	{}
+    }
+}
---
+++ src/main/java/pruebashibernate/myApp/listarCentros.java
@@ -0,0 +1,10 @@
+package pruebashibernate.myApp;
+
+public class listarCentros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
